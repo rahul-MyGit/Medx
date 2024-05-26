007b0ed5b82eb195e822247dc09245981e0eaac1
Rahul <rahul.mymail1@gmail.com>
2024-05-26 11:44:30 +0530

    testing

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen justify-center items-center">
-      <h2 className="font-semibold">medX app</h2>
+      <h2 className="font-semibold">medX ap</h2>
     </main>
   );
 }
