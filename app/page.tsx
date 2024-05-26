36abfcbb9bfd7a82079da0c7da1108ea8d64ce3e
Rahul <rahul.mymail1@gmail.com>
2024-05-26 11:34:38 +0530

    css and deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center">
+    <main className="flex min-h-screen justify-center items-center">
       <h2 className="font-semibold">medX</h2>
     </main>
   );
